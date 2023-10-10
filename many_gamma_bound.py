e7815306d76a0f482b5bbdffad0dee8c27bb9a46
Sam Lobel <samuel.lobel@gmail.com>
2023-10-10 18:27:34 -0400

    updates plotting code

--- many_gamma_bound.py
+++ many_gamma_bound.py
@@ -25,7 +25,7 @@ def test_compute_coefficients_same():
     assert np.allclose(compute_coefficients([0.99], 0.99), [1, ]), compute_coefficients([0.99], 0.99)
     assert np.allclose(compute_coefficients([0.9], 0.9), [1, ]), compute_coefficients([0.9], 0.9)
 
-def make_comparison_plot(known_gammas, gamma_target, coefficients=None, save_path=None):
+def make_comparison_plot(known_gammas, gamma_target, coefficients=None, write_coefficients=False, save_path=None, title=None):
     if coefficients is None:
         coefficients = compute_coefficients(known_gammas, gamma_target)
     print(coefficients)
@@ -38,10 +38,11 @@ def make_comparison_plot(known_gammas, gamma_target, coefficients=None, save_pat
     
     pos_diff, neg_diff = get_difference(known_gammas, coefficients, gamma_target)
 
-    title = fr"-{neg_diff:.4f} < $\Delta$V < {pos_diff:.4f}"
+    # title = fr"-{neg_diff:.4f} < $\Delta$V < {pos_diff:.4f}"
     plt.plot(x_axis, v_orig, label="Original")
     plt.plot(x_axis, v_combined, label="Combined")
-    plt.title(title)
+    if title:
+        plt.title(title)
     plt.legend()
 
     # caption_string = f"Known gammas: {known_gammas}\nTarget gamma: {gamma_target}"
@@ -57,6 +58,23 @@ def make_comparison_plot(known_gammas, gamma_target, coefficients=None, save_pat
                 va='center', # vertical alignment is center
                 bbox=dict(boxstyle='round,pad=0.3', edgecolor='blue', facecolor='aliceblue'),
                 fontsize=12)
+
+    if write_coefficients:
+        plt.subplots_adjust(bottom=0.3)
+        coefficient_string = ", ".join([f"{c:.2f}" for c in coefficients])
+        coefficient_string = f"coefficients: {coefficient_string}"
+        plt.annotate(
+            coefficient_string,
+            # 'This is a \ncentered text box!', 
+            xy=(0.5, -0.4), # (x, y) position with respect to plot
+            xycoords='axes fraction', # use axes fraction coordinates system
+            textcoords='offset points', # use offset points from the specified xy position
+            ha='center', # horizontal alignment is center
+            va='center', # vertical alignment is center
+            bbox=dict(boxstyle='round,pad=0.3', edgecolor='blue', facecolor='aliceblue'),
+            fontsize=12)
+
+
 
     if save_path:
         plt.savefig(save_path)
@@ -85,9 +103,28 @@ def get_difference(known_gammas, coefficients, gamma_target, final_step = 1000):
 
 
 if __name__ == '__main__':
-    # make_comparison_plot([0.9, 0.95], 0.8)
-    # make_comparison_plot([0.85, 0.95], 0.9)
-    make_comparison_plot([0.88, 0.92], 0.9)
-    # make_comparison_plot(np.linspace(0.9, 0.99, 10), 0.8)
-    # make_comparison_plot([0.9, 0.95], 0.9)
-    # make_comparison_plot([0.79, 0.81], 0.8)
+    make_comparison_plot([0.88, 0.92], 0.9, title="Known on Both Sides", save_path="./plots/many_gamma/known_on_both_sides.png", write_coefficients=True)
+    make_comparison_plot(
+        [0.80, 0.85], 0.9, title="Two Much Smaller", save_path="./plots/many_gamma/two_much_smaller.png",
+        write_coefficients=True)
+    make_comparison_plot(
+        [0.80, 0.81, 0.82, 0.83, 0.84, 0.85], 0.9, title="Six All Smaller", save_path="./plots/many_gamma/six_all_smaller.png",
+        write_coefficients=True)
+    make_comparison_plot(
+        [0.85, 0.9], 0.8, title="Two Much Bigger", save_path="./plots/many_gamma/two_much_bigger.png",
+        write_coefficients=True)
+    make_comparison_plot(
+        [0.86, 0.87, 0.88, 0.89, 0.90], 0.8, title="Six All Bigger", save_path="./plots/many_gamma/six_all_bigger.png",
+        write_coefficients=True)
+    make_comparison_plot(
+        [0.86, 0.87, 0.88, 0.89, 0.90], 0.99, title="Approximating 0.99 from way below", save_path="./plots/many_gamma/point_99_from_way_below.png",
+        write_coefficients=True)
+    make_comparison_plot(
+        [0.9, 0.91, 0.92, 0.93, 0.94, 0.95], 0.99, title="Approximating 0.99 from below", save_path="./plots/many_gamma/point_99_from_below.png",
+        write_coefficients=True)
+    make_comparison_plot(
+        [0.96, 0.97, 0.98, ], 0.99, title="Approximating 0.99 from close below", save_path="./plots/many_gamma/point_99_from_close_below.png",
+        write_coefficients=True)
+    make_comparison_plot(
+        [0.98, 0.995], 0.99, title="Approximating 0.99 from both sides", save_path="./plots/many_gamma/point_99_from_both sides.png",
+        write_coefficients=True)
